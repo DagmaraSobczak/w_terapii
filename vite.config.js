9c22ba07d2120d2e951d049cb6c6dcd11b01712e
DagmaraSobczak <daga.sobczak@gmail.com>
2024-08-14 16:53:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/w_terapii',
+  base: '/w_terapii/', // Ustawienie bazowego URL-a aplikacji
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Dodanie rozszerzeń dla lepszej obsługi
+  },
+  esbuild: {
+    loader: 'jsx', // Upewnienie się, że pliki JSX są poprawnie obsługiwane
+    include: /src\/.*\.[jt]sx?$/, // Zastosowanie loadera tylko do plików JSX/TSX w katalogu src
+  },
+  server: {
+    strictPort: true, // Ustawienie, które sprawia, że serwer lokalny używa wyłącznie określonego portu
+  },
 })
